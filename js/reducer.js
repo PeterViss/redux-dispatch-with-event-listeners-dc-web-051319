1d05d9d0121a21345123ae3ab4a473600d3cf754
Peter Viss <peter.c.viss@biola.edu>
2019-10-13 00:19:04 -0400

    creating buttons that add and decrease the number, but does not go below zero

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,36 @@
-// add code snippets from README
+let state;
+let plusButton = document.getElementById("plusButton");
+let minusButton = document.getElementById("minusButton");
+function reducer(state = { count: 0 }, action) {
+  switch (action.type) {
+    case "INCREASE_COUNT":
+      return { count: state.count + 1 };
+    case "DECREASE_COUNT":
+      if (state.count === 0) {
+        alert("Cannot Decrease Below 0!");
+      } else {
+        return { count: state.count - 1 };
+      }
+    default:
+      return state;
+  }
+}
+
+function render() {
+  let container = document.getElementById("container");
+  container.textContent = state.count;
+}
+
+function dispatch(action) {
+  state = reducer(state, action);
+  render();
+}
+
+plusButton.addEventListener("click", () => {
+  dispatch({ type: "INCREASE_COUNT" });
+});
+
+minusButton.addEventListener("click", () => {
+  dispatch({ type: "DECREASE_COUNT" });
+});
+dispatch({ type: "@@INIT" });
